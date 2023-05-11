60bac1a3a8fe5bbadd90ba72632d93633d2c42bc
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-11 13:26:55 +0000

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,52 +11,39 @@ env.hosts = ["54.146.76.82", "54.165.77.174"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
-
-def do_pack():
-    """pack directory tar format to another directory"""
-
-    local('mkdir -p versions')
-    format_time = datetime.now().strftime('%Y%m%d%H%M%S')
-    filepath = 'versions/web_static_{}.tgz'.format(format_time)
-    cmd = "tar -cvzf {} web_static/".format(filepath)
-    try:
-        local(cmd, capture=True)
-        return filepath
-    except:
-        return None
-
-
 def do_deploy(archive_path):
-    """deploy tar package to remote server"""
-
-    if not os.path.isfile(archive_path) and not os.path.exists(archive_path):
-        return False
-
-    put(archive_path, "/tmp")
+    """Archive distributor"""
     try:
-        fileonly = os.path.basename(archive_path)
-        filename = os.path.splitext(fileonly)[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        from_here = "/tmp/{}".format(fileonly)
-        to_here = "/data/web_static/releases/{}/".format(filename)
-        run("tar -xzf {} -C {}".format(from_here, to_here))
-        run('rm /tmp/{}'.format(fileonly))
-        run('mv {}web_static/* {}'.format(to_here, to_here))
-        run('rm -rf {}web_static'.format(to_here))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(to_here))
-        print('New version deployed!')
-        return True
-    except:
+        try:
+            if os.path.exists(archive_path):
+                arc_tgz = archive_path.split("/")
+                arg_save = arc_tgz[1]
+                arc_tgz = arc_tgz[1].split('.')
+                arc_tgz = arc_tgz[0]
+
+                """Upload archive to the server"""
+                put(archive_path, '/tmp')
+
+                """Save folder paths in variables"""
+                uncomp_fold = '/data/web_static/releases/{}'.format(arc_tgz)
+                tmp_location = '/tmp/{}'.format(arg_save)
+
+                """Run remote commands on the server"""
+                run('mkdir -p {}'.format(uncomp_fold))
+                run('tar -xvzf {} -C {}'.format(tmp_location, uncomp_fold))
+                run('rm {}'.format(tmp_location))
+                run('mv {}/web_static/* {}'.format(uncomp_fold, uncomp_fold))
+                run('rm -rf {}/web_static'.format(uncomp_fold))
+                run('rm -rf /data/web_static/current')
+                run('ln -sf {} /data/web_static/current'.format(uncomp_fold))
+                run('sudo service nginx restart')
+                return True
+            else:
+                print('File does not exist')
+                return False
+        except Exception as err:
+            print(err)
+            return False
+    except Exception:
+        print('Error')
         return False
-    return True
-
-
-def deploy():
-    """deploy all"""
-
-    archive_path = do_pack()
-    if not os.path.isfile(archive_path) and not os.path.exists(archive_path):
-        return False
-
-    return do_deploy(archive_path)

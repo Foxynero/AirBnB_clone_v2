734294d7dfa06975ec7d830f2d47496731fe2388
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-19 17:44:15 +0000

    Update console.py

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 import cmd
 import sys
 import re
+from models import storage
+from datetime import datetime
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User

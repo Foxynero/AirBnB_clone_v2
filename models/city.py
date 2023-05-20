b6f9ae6d4ce7462d4cba47d3c4543d30ce636dc8
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-20 01:06:09 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -14,8 +14,14 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+
+    __tablename__ = 'cities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey("states.id"),
+                          nullable=False)
+        places = relationship("Place", cascade='all')
+    else:
+        state_id = ""
+        name = ""

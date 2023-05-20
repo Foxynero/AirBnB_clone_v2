749b799faed83f52e4a2024444ac3c741cd28d62
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-20 00:55:25 +0000

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,74 +1,78 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
-from os import getenv
-import models
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+"""This is the file storage class for AirBnB"""
+import json
 from models.base_model import BaseModel, Base
+from models.user import User
 from models.state import State
 from models.city import City
-from models.user import User
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.amenity import Amenity
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
 
 
-class DBStorage():
-    """new storge"""
+class DBStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        """initialization"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
+        """
+        Create the engine(self.__engine)
+        """
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+            getenv("HBNB_MYSQL_USER"), getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB')),
                                       pool_pre_ping=True)
-        if HBNB_ENV == "test":
-            Base.metadata.drop_all(self.__engine)
+        if getenv("HBNB_MYSQL_ENV") == "test":
+            Base.metadata.drop_all()
 
     def all(self, cls=None):
-        """squery database session"""
-        all_dict = {}
-        for itr in classes:
-            if cls is None or cls == itr:
-                objs = self.__session.query(classes[itr]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    all_dict[key] = obj
-        return (all_dict)
+        """
+        Returns a dictionary
+        """
+        my_objects = dict()
+        objects = ["State", "City", "User", "Place", "Review", "Amenity"]
+
+        for objts in objects:
+            for obj in self.__session.query(eval(objts)).all():
+                my_objects[type(obj).__name__+"."+obj.id] = obj
+
+        return(my_objects)
 
     def new(self, obj):
-        """add new obj"""
-        if obj is not None:
-            self.__session.add(obj)
-            self.save()
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        self.__session.add(obj)
 
     def save(self):
-        """commit changes"""
+        """serialize the file path to JSON file path
+        """
         self.__session.commit()
 
-    def delete(self, obj=None):
-        """delete obj if exists"""
-        if obj is not None:
-            del obj
-            self.save()
-
     def reload(self):
-        """reload"""
+        """serialize the file path to JSON file path
+        """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(sessionmaker(expire_on_commit=False,
+                                        bind=self.__engine))()
 
+    def delete(self, obj=None):
+        """
+        Deletes an object from objects
+        """
+        if obj:
+            self.__session.delete(obj)
+    
     def close(self):
-        """ calls close() on the class Session """
+        """more methods"""
         self.__session.close()
